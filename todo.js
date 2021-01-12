0a73ab6a4f57161936e6313021e8b5a35f315c23
Roberta Santos <robertasantosrj@gmail.com>
2021-01-12 00:07:20 -0300

    *todo.js

--- todo.js
+++ todo.js
@@ -4,15 +4,15 @@ const app = express();
 
 const port = 3001;
 
-app.get('/usuario', (req, resp)=> {
+app.get(`/usuario`, (req, resp)=> {
     resp.send('Rota ativada com GET e recurso usuario')
 });
 
-app.get('/tarefa', (req, resp)=> {
-    resp.send('Rota ativada com GET e recurso tarefa')
+app.listen(port, ()=> {
+    console.log(`[INFO]: app rodando na porta: ${port}`)
 });
 
-app.listen(port, ()=> {
-    console.log(`[INFO]: app rodando na porta http://localhost: ${port}`)
+app.get(`/tarefas`, (req, resp)=> {
+    resp.send('Rota ativada com GET e recurso tarefas')
 });
 
