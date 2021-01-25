f1e03fab7bbaca3aa3523428d9c3990bdbbe8695
Roberta Santos <robertasantosrj@gmail.com>
2021-01-25 17:58:56 -0300

    +user routes: delete, update

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const usuariosController = require('./controllers/usuarios-controller');
 const tarefasController = require('./controllers/tarefas-controller');
-
+const bd = require('./infra/bd')
 const port = 8080;
 
 const app = express();
@@ -10,8 +10,8 @@ const app = express();
 app.use(bodyParser.json());
 
 
-usuariosController(app);
-tarefasController(app);
+usuariosController(app,bd);
+tarefasController(app, bd);
 
 
 app.listen(port, ()=>{
--- src/controllers/tarefas-controller.js
+++ src/controllers/tarefas-controller.js
@@ -1,11 +1,39 @@
-module.exports = (app)=> {
-    app.get('/tarefas', (req, resp)=> {
-    resp.send('<h2>Rota GET de tarefas ativada: rastreamento da aplicação sendo feita com nodemon</h2>')
+const Tarefa = require('../models/tarefa');
+
+module.exports = (app, bd)=> {
+    app.get('/tarefas', (req, resp)=> {   
+        resp.send(bd.tarefas);
     });
 
-    app.post('/tarefas', (req, resp)=> {
-        
-        console.log(`Request body issues:${req.body.curso}`)
-        resp.send('<h2>Rota POST ok!</h2>')
+    app.get('/tarefas/:titulo', (req, resp)=> {
+        for(let trf of bd.tarefas){
+            if(trf.titulo == req.params.titulo){
+                resp.send(trf)
+            }
+        }
+        resp.send('Tarefa não encontrada!')
     });
+
+    app.get('/tarefas/:dataCriacao', (req, resp)=> {
+        for(let trf of bd.tarefas){
+            if(trf.dataCriacao == req.params.dataCriacao){
+                resp.send(trf)
+            }
+        }
+        resp.send('Tarefa não encontrada!')
+    });
+
+
+    app.post('/tarefas', (req, resp)=> {
+        //utilizar o body da requisição para criar nova tarefa!
+        const trf = new Tarefa(req.body.titulo, req.body.descricao, req.body.status, req.body.dataCriacao);
+        //para guardar as tarefas no array
+        bd.tarefas.push(trf);
+        console.log(trf);
+        resp.send('<h2>Rota POST de tarefa ok!</h2>');
+    });
+
+    // app.delete('/tarefas', (_req, resp)=> {
+    //     resp.send('Recebi uma solicitação de DELETE na rota /tarefas')
+    // });
 }
