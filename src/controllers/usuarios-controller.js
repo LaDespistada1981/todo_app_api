c90906f61a160c35328be8119db855f89c1e1e32
Roberta Santos <robertasantosrj@gmail.com>
2021-01-26 02:55:39 -0300

    *gitignores, *usuarios-controller.js

--- src/controllers/usuarios-controller.js
+++ src/controllers/usuarios-controller.js
@@ -12,6 +12,41 @@ module.exports = (app,bd)=> {
         //para guardar os usuários no array
         bd.usuarios.push(usr);
         console.log(bd);
+<<<<<<< Updated upstream
         resp.send('Usuário cadastrado ok!');
+=======
+        resp.send('Usuário cadastrado no banco de dados!');
+    });
+
+
+    app.delete('/usuarios/:email', (req, resp)=>{
+        //deleção de elemento no bd
+        let usrNaoDelet = [];
+        for(let i =0; i< bd.usuarios.length; i++){
+            if(bd.usuarios[i].email !== req.params.email){
+                usrNaoDelet.push(bd.usuarios[i])
+            }
+        }
+
+        bd.usuarios = usrNDelet;
+        console.log(bd.usuarios)
+        resp.send(`Rota delete ativada! - Implementação pendente`)
+    });
+
+    //função criada para ser implementada na rota Put
+    const atualizaRegistro = (email,body)=>{
+        for(let usr of bd.usuarios){
+            if(usr.email === email){
+                usr.nome = body.nome;
+                usr.senha = body.senha;
+            }
+        }
+    }
+
+    app.put('/usuarios/:email', (req, resp)=>{
+    
+        atualizaRegistro(req.params.email, req.body);
+        resp.send("Usuário atualizado!")
+>>>>>>> Stashed changes
     })
 } 
